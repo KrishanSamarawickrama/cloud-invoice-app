3f5f7f54adb709f788d980afa2c2b24f978fd427
Krishan Samarawickrama <Krishans1990@live.com>
2025-02-24 22:07:40 +0530

    Refactor invoice date handling and add invoice list endpoint

---
+++ src/CloudInvoiceApp.Backend/Endpoints/GetInvoiceListEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using CloudInvoiceApp.Backend.Data;
+using CloudInvoiceApp.Backend.Requests;
+using CloudInvoiceApp.Backend.Responses;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudInvoiceApp.Backend.Endpoints;
+
+public class GetInvoiceListEndpoint(InvoiceDbContext dbContext) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/api/invoices/list");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var invoices = await dbContext.Invoices.ToListAsync(ct);
+        var invoicesResponse = invoices.Select(x => new InvoiceListResponse
+            {
+                Id = x.Id,
+                FileName = x.FileName,
+                InvoiceNumber = x.InvoiceNumber,
+                InvoiceDate = x.InvoiceDate,
+                Amount = JsonSerializer.Deserialize<List<Service>>(x.Services)!.Sum(x=>x.Amount)
+            });
+        
+        await SendOkAsync(invoicesResponse, ct);
+    }
+}
--- src/CloudInvoiceApp.Backend/Responses/InvoiceResponse.cs
+++ src/CloudInvoiceApp.Backend/Responses/InvoiceResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace CloudInvoiceApp.Backend.Requests;
+﻿using CloudInvoiceApp.Backend.Requests;
 
-public sealed record UpdateInvoiceRequest
+namespace CloudInvoiceApp.Backend.Responses;
+
+public sealed class InvoiceResponse
 {
     public int Id { get; set; }
     public string? FileName { get; set; }
     public required string InvoiceNumber { get; set; } = "00-000013";
-    public required DateOnly InvoiceDate { get; set; }
+    public required string InvoiceDate { get; set; }
     public required List<string> From { get; set; }
     public required List<string> To { get; set; }
     public required BankDetails BankDetails { get; set; }
